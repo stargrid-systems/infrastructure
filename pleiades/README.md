34b054d5b8913851d3f068d91c2813a1150be271
Simon <simon@siku2.io>
2026-01-12 23:57:02 +0100

    feat: set up kubernetes cluster (#5)
    * temp: add talos-based cluster
    
    * temp: let's manually create the talos cluster then
    
    * temp
    
    * update docs

---
+++ pleiades/README.md
@@ -0,0 +1,26 @@
+# Pleiades Talos Kubernetes cluster
+
+## Bootstrapping
+
+```bash
+talosctl gen secrets -o secrets.yaml
+```
+
+```bash
+CLUSTER_NAME='pleiades.stargrid.systems'
+API_ENDPOINT='https://kube.pleiades.stargrid.systems:6443'
+talosctl gen config "${CLUSTER_NAME}" "${API_ENDPOINT}" --with-secrets secrets.yaml --with-examples=false --with-docs=false --config-patch @patch.yaml
+```
+
+Once Terraform has applied the C1 node:
+
+```bash
+: "${PLEIADES_C1_IP:?}"
+talosctl apply-config --nodes "${PLEIADES_C1_IP}" --insecure --file ./controlplane.yaml
+export TALOSCONFIG=./talosconfig
+talosctl bootstrap --nodes "${PLEIADES_C1_IP:?}"
+talosctl kubeconfig --nodes "${PLEIADES_C1_IP:?}"
+```
+
+Kubernetes is now up and running although not functional yet.
+Head over to <https://github.com/stargrid-systems/pleiades-gitops> now.
---
+++ terraform/pleiades.tf
@@ -0,0 +1,69 @@
+# This snapshot is created from this repository.
+# See: <packer/talos/README.md> for more information.
+data "hcloud_image" "talos" {
+  with_selector = "os=talos"
+  most_recent   = true
+}
+
+resource "hcloud_firewall" "pleiades" {
+  name = "pleiades"
+
+  rule {
+    description = "Allow traffic to Kubernetes API server"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "6443"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    description = "Allow traffic to Talos API"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "50000"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+}
+
+resource "hcloud_server" "pleiades_c1" {
+  name        = "pleiades-c1"
+  server_type = "cx23"
+  image       = data.hcloud_image.talos.id
+  location    = var.default_location
+  ssh_keys    = []
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+  firewall_ids = [hcloud_firewall.pleiades.id]
+
+
+  lifecycle {
+    ignore_changes = [
+      # Talos is self-updating
+      image
+    ]
+  }
+}
+
+resource "cloudflare_dns_record" "pleiades_ipv4" {
+  zone_id = cloudflare_zone.stargrid_systems.id
+  type    = "A"
+  name    = "kube.pleiades.stargrid.systems"
+  content = hcloud_server.pleiades_c1.ipv4_address
+  ttl     = 60
+}
+
+resource "cloudflare_dns_record" "pleiades_ipv6" {
+  zone_id = cloudflare_zone.stargrid_systems.id
+  type    = "AAAA"
+  name    = "kube.pleiades.stargrid.systems"
+  content = hcloud_server.pleiades_c1.ipv6_address
+  ttl     = 60
+}
